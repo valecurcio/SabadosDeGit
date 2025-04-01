6d3ad25eac59c0d6340b16435f21b985bcfb4e08
valecurcio <vale.s.curcio@gmail.com>
2025-04-01 20:21:53 -0300

    test-add-branch

---
+++ claseuno/index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/contacto', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/contacto.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server on port 3000');
+});
+
